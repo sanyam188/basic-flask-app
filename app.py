38b6a839634cf257548f068a48c001aba449f0d7
sanyam188 <sanyamjain.188@gmail.com>
2020-03-28 11:06:23 +0530

    index template created

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask (__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Prakriti"
+    return render_template('index.html')
+    return "Hello, Sanyam"
 
 if __name__=="__main__":
     app.run(debug=True)
